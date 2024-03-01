9a0e65d1eacaa73d2161eccd8ad634456ec7a3ae
hugelinkfan15 <158606627+hugelinkfan15@users.noreply.github.com>
2024-03-01 12:20:00 -0600

    Fixed ScoreManager for real this time
    Can restart the scene whenever now

--- Retro Renegades Platformer/Assets/Scripts/ScoreManager.cs
+++ Retro Renegades Platformer/Assets/Scripts/ScoreManager.cs
@@ -8,16 +8,14 @@ public class ScoreManager : MonoBehaviour
 {
     public static bool gameOver;
     public static bool won;
-    public static int score;
     // Start is called before the first frame update
 
     public TMP_Text textbox;
-    public int scoreToWin;
+
     void Start()
     {
         gameOver = false;
         won = false;
-        score = 0;
     }
 
     // Update is called once per frame
@@ -27,29 +25,11 @@ public class ScoreManager : MonoBehaviour
          {
              textbox.text = "Press R to Restart";
          }
-         if (score >= scoreToWin)
-         {
-             won = true;
-             gameOver = true;
-         }
 
-         if(gameOver)
-         {
-             if (won)
-             {
-                 textbox.text = "You win!\nPress R to Try Again!";
-             }
-             else
-             {
-                 textbox.text = "You lose!\nPress R to Try Again!";
-             }
-             if(Input.GetKeyDown(KeyCode.R))
-             {
-                 SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-             }
-         }
-
-
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
 
     }
 }
