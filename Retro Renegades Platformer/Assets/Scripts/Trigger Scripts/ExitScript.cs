70dc79938538d862b06dbfdd9ca57cc02d2b2dfc
Duncan Barner <duncan.barner@gmail.com>
2024-03-08 11:42:24 -0600

    You Win
    -Added "You Win!" Screen when player interacts with triggerzone in final level
    -Change title screen title and color

--- Retro Renegades Platformer/Assets/Scripts/Trigger Scripts/ExitScript.cs
+++ Retro Renegades Platformer/Assets/Scripts/Trigger Scripts/ExitScript.cs
@@ -1,25 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ExitScript : MonoBehaviour
 {
+
+    public TMP_Text winText;
+    public bool isFinalLevel = false;
     // Start is called before the first frame update
     void Start()
     {
-
+        winText.gameObject.SetActive(false);
     }
     public void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Player")
         {
+            if (isFinalLevel)
+            {
+                ShowWinText();
+            }
+            else
+            {
 
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
         }
 
 
+    }
+    void ShowWinText()
+    {
+        winText.gameObject.SetActive(true);
     }
 
 
