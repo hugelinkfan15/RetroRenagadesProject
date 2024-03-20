67f5a8b3b61d4b6af1f230616cd14e00cbbcd6e0
Duncan Barner <duncan.barner@gmail.com>
2024-03-20 13:08:29 -0500

    Movement Mechanics
    -Adjusted Movement Mechanics
    (added slight coyote time so player can just barely jump even when they're slightly off of a platform, added fallSpeed so that the player falls faster downward which makes the jump feel less floaty, added ability to press spacebar for a small jump and hold spacebar for a large jump)
    -Camera follows player now
    -Levels redesigned to allow for camerafollowplayer
    -Slight UI adjustments

--- Retro Renegades Platformer/Assets/Scripts/PlatformPlayerController.cs
+++ Retro Renegades Platformer/Assets/Scripts/PlatformPlayerController.cs
@@ -4,59 +4,75 @@ using UnityEngine;
 
 public class PlatformPlayerController : MonoBehaviour
 {
-
     public float moveSpeed = 5f;
     public float jumpForce = 10f;
     public LayerMask groundLayer;
     public Transform groundCheck;
-    public float groundCheckRadius = 0.2f; 
+    public float groundCheckRadius = 0.2f;
+    public float coyoteTime = 0.1f;
+    public float variableJumpHeightMultiplier = 0.5f;
+    public float fallingSpeed = 2f;
 
     private Rigidbody2D rb;
     private bool isGrounded;
     private float horizontalInput;
+    private bool isJumping;
+    private float jumpTimeCounter;
 
     [SerializeField] private AudioClip jumpSoundClip;
-    // Start is called before the first frame update
+
     void Start()
     {
-        //Get the Rigidbody2D component attached to the GameObject
         rb = GetComponent<Rigidbody2D>();
-
-        //Ensure the groundCheck variable is assigned
-        if ( groundCheck == null)
+        if (groundCheck == null)
         {
             Debug.LogError("GroundCheck not assigned to the player controller!");
         }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //Get input values for horizontal movement
         horizontalInput = Input.GetAxis("Horizontal");
 
-        //Check for jump input
-        if(Input.GetButtonDown("Jump") && isGrounded && !PauseMenu.isPaused)
-        {
-            //Apply an upward force for jumping
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-
-            SoundFXManager.instance.PlaySoundFXCLip(jumpSoundClip,transform, 1f);
-            
-        }
-        
-    }
-
-    private void FixedUpdate()
-    {
-        //Move the player using Rigidbody2D in FixedUpdate
-        rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
-
-        //Check if player is grounded
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
 
-        //Optionally, you can add animations or other behavior here based on player state
-        if(horizontalInput > 0 && !PauseMenu.isPaused)
+        // Coyote Time: Allow jumping shortly after leaving the ground
+        if (isGrounded)
+        {
+            jumpTimeCounter = coyoteTime;
+        }
+        else
+        {
+            jumpTimeCounter -= Time.deltaTime;
+        }
+
+        // Check for jump input
+        if (Input.GetButtonDown("Jump") && (isGrounded || jumpTimeCounter > 0) && !PauseMenu.isPaused)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            isJumping = true;
+            jumpTimeCounter = 0;
+            SoundFXManager.instance.PlaySoundFXCLip(jumpSoundClip, transform, 1f);
+        }
+
+        // Variable Jump Height: Allow higher jumps by holding the jump button
+        if (Input.GetButtonUp("Jump") && isJumping)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * variableJumpHeightMultiplier);
+            isJumping = false;
+        }
+        if (rb.velocity.y < 0 && !isGrounded)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallingSpeed - 1) * Time.deltaTime;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
+
+        // Flip the player sprite based on movement direction
+        if (horizontalInput > 0 && !PauseMenu.isPaused)
         {
             transform.localScale = new Vector3(2f, 2f, 1f); // Facing right
         }
@@ -64,6 +80,12 @@ public class PlatformPlayerController : MonoBehaviour
         {
             transform.localScale = new Vector3(-2f, 2f, 1f); // Facing Left
         }
-       
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        // Visualize the ground check radius in the Scene view
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
     }
 }
